8645fb3e2999e958afcc04ba316e0cf1841c8593
Plopmenz <plopmenz@gmail.com>
2025-04-21 13:06:42 +0200

    install through requirements.txt

--- nix/python-app.nix
+++ nix/python-app.nix
@@ -33,7 +33,7 @@ python3Packages.buildPythonApplication {
       #!/bin/sh
       ${python} -m venv venv
       source venv/bin/activate
-      pip install uagents aiohttp aiohttp-cors torch torchaudio
+      pip install -r $out/bin/src/requirements.txt
       export PORT=8000
       python $out/bin/src/app.py
       EOF
