7d015ea9874638a655645eeceae7d8560967223b
Plopmenz <plopmenz@gmail.com>
2025-04-29 21:07:24 +0200

    match frontend and backend prompts

--- nextjs-app/app/mimic/page.tsx
+++ nextjs-app/app/mimic/page.tsx
@@ -196,9 +196,9 @@ export default function SpeakerPage() {
         <div className="grow text-5xl flex place-items-center place-content-center">
           <span>
             Hello there, I am mimic. I will copy your voice and give myself
-            several compliments. I am not quite sure what text we should put
-            here, anything less than 30 seconds should work. Are you excited for
-            the result? Make sure the content has a similar vibe all the way
+            several compliments. Are you excited for the result? I am not quite
+            sure what text we should put here, anything less than 30 seconds
+            should work. Make sure the content has a similar vibe all the way
             through that matches our output prompt.
           </span>
         </div>
